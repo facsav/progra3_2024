6a9f12ac012d42b49e474eba497c1de58ac96982
NicoHernandezUade <nihernandez@uade.edu.ar>
2024-09-06 10:47:48 -0300

    Agrego denominaciones a act2_clase4 y creo archivos de clase5

--- src/clase4/act2_clase4.java
+++ src/clase4/act2_clase4.java
@@ -57,9 +57,9 @@ public class act2_clase4 {
     public static void main(String[] args) {
         Comprobante c1 = new Comprobante("bono", 10000);
         Comprobante c2 = new Comprobante("cheque", 5000);
-        Comprobante c3 = new Comprobante("moneda50", 0.50);
-        Comprobante c4 = new Comprobante("moneda25", 0.25);
-        Comprobante c5 = new Comprobante("moneda1", 0.01);
+        Comprobante c3 = new Comprobante("moneda050", 0.50);
+        Comprobante c4 = new Comprobante("moneda025", 0.25);
+        Comprobante c5 = new Comprobante("moneda01", 0.01);
         Comprobante c6 = new Comprobante("billete100", 100);
         Comprobante c7 = new Comprobante("billete1000", 1000);
         Comprobante c8 = new Comprobante("billete2000", 2000);
@@ -67,8 +67,12 @@ public class act2_clase4 {
         Comprobante c10 = new Comprobante("billete500", 500);
         Comprobante c11 = new Comprobante("billete50", 50);
         Comprobante c12 = new Comprobante("billete20", 20);
+        Comprobante c13 = new Comprobante("moneda1", 1);
+        Comprobante c14 = new Comprobante("moneda2", 2);
+        Comprobante c15 = new Comprobante("moneda5", 10);
+        Comprobante c16 = new Comprobante("moneda10", 20);
 
-        Comprobante[] comprobantes = {c1, c2, c3, c4, c5, c6, c7, c8, c9, c10, c11, c12};
+        Comprobante[] comprobantes = {c1, c2, c3, c4, c5, c6, c7, c8, c9, c10, c11, c12,c13,c14,c15,c16};
         MonedaExtranjera m1 = new MonedaExtranjera("dolar", 1257.76);
         MonedaExtranjera m2 = new MonedaExtranjera("euro", 1500.43);
         MonedaExtranjera m3 = new MonedaExtranjera("libra", 2000.91);
---
+++ src/clase5/act3_clase5.java
@@ -0,0 +1,15 @@
+package clase5;
+
+public class act3_clase5 {
+    /*
+     * Diseño de una red de distribución eléctrica
+Una empresa de energía necesita conectar varias estaciones eléctricas en una
+región para asegurar que toda la zona esté alimentada de manera eficiente. Las
+estaciones están ubicadas en diferentes ciudades y los costos de instalación de
+las líneas eléctricas entre ellas varían según la distancia y el terreno.
+Tareas:
+Representar el grafo utilizando una lista de adyacencia.
+Aplicar el algoritmo de Prim para determinar el Árbol de Recubrimiento Mínimo.
+Mostrar el conjunto de conexiones resultante y calcular el costo total.
+     */
+}
